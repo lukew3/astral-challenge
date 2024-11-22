32ea9e618d3b133d1b8acd4c5b8b167be5abfd24
Luke Weiler <lukew25073@gmail.com>
2024-11-22 18:32:46 -0500

    add webcam capture

---
+++ src/app/_components/webcamCapture.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React from "react";
+import Webcam from "react-webcam";
+import { useCallback, useState } from "react";
+
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: "user"
+};
+
+const WebcamCapture = () => {
+  const webcamRef = React.useRef<Webcam>(null);
+  const [url, setUrl] = useState<string | null>(null);
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current?.getScreenshot();
+    if (imageSrc) {
+      setUrl(imageSrc);
+      console.log(imageSrc);
+    }
+  }, [webcamRef]);
+
+  return (
+    <>
+      <Webcam
+        audio={false}
+        height={720}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={1280}
+        videoConstraints={videoConstraints}
+      />
+      <button onClick={capture}>Capture photo</button>
+    </>
+  );
+};
+  
+export default WebcamCapture;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import Link from "next/link";
-
-import { LatestPost } from "~/app/_components/post";
 import { api, HydrateClient } from "~/trpc/server";
+import React from "react";
 import styles from "./index.module.css";
+import WebcamCapture from "~/app/_components/webcamCapture";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
@@ -12,42 +11,8 @@ export default async function Home() {
   return (
     <HydrateClient>
       <main className={styles.main}>
-        <div className={styles.container}>
-          <h1 className={styles.title}>
-            Create <span className={styles.pinkSpan}>T3</span> App
-          </h1>
-          <div className={styles.cardRow}>
-            <Link
-              className={styles.card}
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className={styles.cardTitle}>First Steps →</h3>
-              <div className={styles.cardText}>
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className={styles.card}
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className={styles.cardTitle}>Documentation →</h3>
-              <div className={styles.cardText}>
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
-          <div className={styles.showcaseContainer}>
-            <p className={styles.showcaseText}>
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
-          </div>
-
-          <LatestPost />
-        </div>
+        <span className={styles.header}>Math Made Easy</span>
+        <WebcamCapture />
       </main>
     </HydrateClient>
   );
